fd23bc5a3d48579c5485994abc53739f573a93ee
AtulPant2704 <atulpant1999@gmail.com>
2022-06-11 14:51:36 +0530

    refactor: 50 products in on network call

--- src/redux/asyncThunks/currenciesThunk.js
+++ src/redux/asyncThunks/currenciesThunk.js
@@ -4,7 +4,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 const getCurrencies = createAsyncThunk("getCurrencies", async (pageNumber) => {
   try {
     const response = await axios.get(
-      `https://api.coincap.io/v2/assets/?limit=${pageNumber * 5}`
+      `https://api.coincap.io/v2/assets/?limit=${pageNumber * 50}`
     );
     const data = { data: response.data.data, status: response.status };
     return data;
